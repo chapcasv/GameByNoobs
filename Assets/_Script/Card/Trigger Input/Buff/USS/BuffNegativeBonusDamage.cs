31f81d3b786fa25001089506b55f2d7550c01148
tienmanh99 <tienmanhvh999@gmail.com>
2022-01-27 21:44:23 +0700

    create Unit card and item

---
+++ Assets/_Script/Card/Trigger Input/Buff/USS/BuffNegativeBonusDamage.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PH
+{
+    [CreateAssetMenu(fileName = "BSS", menuName = "ScriptableObject/Card/Trigger Input/Buff/Negative Bonus Damage")]
+    public class BuffNegativeBonusDamage : Buff
+    {
+        public override void Excute(BaseUnit unit)
+        {
+            if (buffOneRound)
+            {
+                OneRound(unit);
+            }
+            else
+            {
+                EndLess(unit);
+            }
+        }
+
+        private void EndLess(BaseUnit unit)
+        {
+            var USS = unit.GetUnitSurvivalStat;
+            USS.BuffNegative(true);
+        }
+
+        private void OneRound(BaseUnit unit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
