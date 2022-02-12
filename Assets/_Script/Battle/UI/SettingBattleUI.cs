c9ffeb8b7ddd7f6e0d1eadc57da825485f8bfe89
chapcasv <87607750+chapcasv@users.noreply.github.com>
2022-02-12 16:08:55 +0700

    Reload

--- Assets/_Script/Battle/UI/SettingBattleUI.cs
+++ Assets/_Script/Battle/UI/SettingBattleUI.cs
@@ -1,99 +1,23 @@
-using DG.Tweening;
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 using UnityEngine.UI;
+using System;
 
 namespace PH
 {
     public class SettingBattleUI : UIScreen
     {
-        [System.Serializable]
-        public class TopButtonTap
-        {
-            public Button main;
-            public GameObject selectImg;
-            public Action OnSelect;
-
-            public void Initialize()
-            {
-                main.onClick.AddListener(OnClickCallBack);
-            }
-
-            private void OnClickCallBack()
-            {
-                selectImg.gameObject.SetActive(true);
-                OnSelect?.Invoke();
-            }
-            public void DeSelect()
-            {
-                selectImg.gameObject.SetActive(false);
-            }
-        }
         [SerializeField] private GameObject main;
         [SerializeField] private GameObject Overlay;
-        [Header("Bot Button")]
         [SerializeField] private Button closeButton;
-        [SerializeField] private Button supportButton, exitGameButton, surdererButton;
-        
-
-        [Header("Top Button Tab")]
-        [SerializeField] private TopButtonTap generalButtonTab;
-        [SerializeField] private TopButtonTap musicButtonTab;
-        [SerializeField] private TopButtonTap audioButtonTab; 
-        
 
         public override void Initialize()
         {
             base.Initialize();
             closeButton.onClick.AddListener(OnCloseSettingCallBack);
-            supportButton.onClick.AddListener(OnSupportButtonCallBack);
-            exitGameButton.onClick.AddListener(OnExitgameCallBack);
-            surdererButton.onClick.AddListener(OnSurdererCallBack);
-
-
-            generalButtonTab.Initialize();
-            audioButtonTab.Initialize();
-            musicButtonTab.Initialize();
-
-            generalButtonTab.OnSelect = OnGeneralSetting;
-            musicButtonTab.OnSelect = OnMusicSetting;
-            audioButtonTab.OnSelect = OnAudioSetting;
-           
-
         }
-
-        private void OnSurdererCallBack()
-        {
-            throw new NotImplementedException();
-        }
-
-       
-
-        private void OnSupportButtonCallBack()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void OnGeneralSetting()
-        {
-            musicButtonTab.DeSelect();
-            audioButtonTab.DeSelect();
-
-        }
-        private void OnMusicSetting()
-        {
-            generalButtonTab.DeSelect();
-            audioButtonTab.DeSelect();
-        }
-
-
-        private void OnAudioSetting()
-        {
-            generalButtonTab.DeSelect();
-            musicButtonTab.DeSelect();
-        }
-
-
 
         private void OnCloseSettingCallBack()
         {
@@ -108,7 +32,7 @@ namespace PH
         }
         public override void Hide()
         {
-
+          
             main.transform.localScale = Vector3.one;
             main.transform.DOScale(0.0f, 0.3f).OnComplete(
                 () =>
@@ -117,11 +41,7 @@ namespace PH
                 }
                 );
         }
-        private void OnExitgameCallBack()
-        {
-            Application.Quit();
-        }
-
+      
 
     }
 
